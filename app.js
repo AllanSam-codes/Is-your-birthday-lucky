03f56529d4b28ace00fa49d419fe2052a1dca288
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-13 23:35:20 +0530

    updated styles

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
 
-checkNumberButton.addEventListener('click', function getValues() {
-    console.log(dateOfBirth.value, luckyNumber.value);
-})
+
+function compareValues(sum, luckyNumber) {
+    //console.log(sum, luckyNumber)
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "your birthday is lucky 🤩"
+    } else {
+        outputBox.innerText = "your birthday is not lucky😢"
+    }
+}
+
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value
+    const sum = calculateSum(dob)
+    if (sum && dob) {
+        compareValues(sum, luckyNumber.value);
+    } else {
+        outputBox.innerText = "please enter both the fields 😮"
+    }
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "")
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index))
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
